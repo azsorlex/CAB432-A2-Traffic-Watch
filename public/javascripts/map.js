c1989f0da92a5d07b2d8774d3dd8f267da45f114
azsorlex <arozsa@protonmail.com>
2020-10-10 20:56:09 +1000

    Added Chart.js to the project
    Also added an example

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,6 +8,12 @@ function initMap() {
   // Get the data that was passed through from the server
   const webcamData = JSON.parse(document.getElementById("mapScript").getAttribute("webcamData"));
 
+  // Add a click event to the graph housing element which will hide itself
+  const webcamoverlay = document.getElementById("webcamoverlay");
+  webcamoverlay.addEventListener('click', function () {
+    webcamoverlay.style.display = "none";
+  });
+
   // Close the current info window when the map is clicked
   map.addListener('click', function (mapsMouseEvent) {
     closeCurrentInfoWindow();
@@ -17,25 +23,21 @@ function initMap() {
     // Add a marker for the webcam to the map
     const marker = new google.maps.Marker({
       map,
-      position: {lat: cam.geometry.coordinates[1], lng: cam.geometry.coordinates[0]},
+      position: { lat: cam.geometry.coordinates[1], lng: cam.geometry.coordinates[0] },
       title: cam.properties.description
     });
 
     // Create an Info Window that appears when the marker is clicked on
     const infowindow = new google.maps.InfoWindow({
       content:
-        //`<img src="${cam.properties.image_url}" alt="Still image of the camera"</img>` +
-        //`<video id="streamPlayer">` +
-        //`<source src="${cam.properties.url}" type="rtmp/mp4"</source>` +
-        //`</video>` +
+        `<img id="${cam.properties.id}" src="${cam.properties.image_url}" alt="Refreshing"</img>` +
         `<h2>${cam.properties.description}</h2>` +
-        
         `<p>Direction: ${cam.properties.direction}</p>` +
-        `<a href=# onclick="javascript:queryOpenCV('${cam.properties.id}')">View live feed</a>`
+        `<a href=# onclick="javascript:displayGraph('${cam.properties.id}')">View past data</a>`
     });
 
     // Open the info window when the marker is clicked
-    marker.addListener('click', function() {
+    marker.addListener('click', function () {
       closeCurrentInfoWindow();
       currentInfoWindow = infowindow;
       infowindow.open(map, marker);
@@ -49,16 +51,42 @@ function closeCurrentInfoWindow() {
   } catch (e) { }
 }
 
-function queryOpenCV(webcamID) {
-  // Query OpenCV, then do something with the response
-  fetch(`/opencv/${webcamID}`)
+function queryOpenCV(imageURL, imageID) {
+  fetch(`/opencv/${imageURL.replace(/\//g, '$')}`) // Alter the URL so that all / are replaced with $
     .then((res) => res.json())
     .then((data) => {
+      document.getElementById(imageID).src = data; // An example for how to update the image
+    })
+    .catch((error) => console.log(error))
+}
 
+// Query the server to get the results for a particular camera, then display them
+function displayGraph(id) {
+  const ctx = document.getElementById('mychart').getContext('2d');
+  fetch(`/opencv/graph/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }]
+        },
+
+        // Configuration options go here
+        options: {}
+      });
     })
     .catch((error) => console.log(error));
 
   // Show the window where the detailed information will be displayed
-  const webcamoverlay = document.getElementById("webcamoverlay");
   webcamoverlay.style.display = "flex";
 }
--- routes/opencv.js
+++ routes/opencv.js
@@ -6,12 +6,12 @@ const cv = require('opencv4nodejs');
 require('dotenv').config();
 
 /* Process the webcam's feed and send the results back to the client */
-router.get('/:id', function (req, res, next) { 
-    const url = `https://api.qldtraffic.qld.gov.au/v1/webcams/${req.params.id}`;
+router.get('/:url', function (req, res, next) { 
+    const url = req.params.url.replace(/\$/g, '/'); // Change the $ back into /
     console.log(url);
 
     // Got the following from the opencv4nodejs NPM webpage. There's a possibility we can't use this.
-    const wCap = new cv.VideoCapture(url);
+    /*const wCap = new cv.VideoCapture(url);
     const delay = 10;
     let done = false;
     while (!done) {
@@ -24,7 +24,15 @@ router.get('/:id', function (req, res, next) {
 
         const key = cv.waitKey(delay);
         done = key !== 255;
-    }
+    }*/
+
+    
+});
+
+router.get('/graph/:id', function (req, res, next) { 
+    const { id } = req.params;
+
+    res.json([{}]); // Just a blank response
 });
 
 module.exports = router;
