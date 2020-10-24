98e1a79922781e17ae75049c7212457bec62db67
azsorlex <arozsa@protonmail.com>
2020-10-24 15:26:44 +1000

    Surrounded image processing in a try-catch

--- routes/tensorflow.js
+++ routes/tensorflow.js
@@ -28,7 +28,7 @@ const ip = "http://localhost:3000"; // Use this for local development
 const cache = redis.createClient(); // Use this for local development
 //const cache = redis.createClient(6379, "alex-ethan-ass2-cache.km2jzi.ng.0001.apse2.cache.amazonaws.com");
 
-crontab.scheduleJob("30 15 * * *", () => { // Create a job that runs at midnight that queries the QLDTraffic API and creates a new cron job for every camera returned
+crontab.scheduleJob("0 16 * * *", () => { // Create a job that runs at midnight that queries the QLDTraffic API and creates a new cron job for every camera returned
   s3.listObjects({ Bucket: bucketName }, function (err, data) {
     let items = [];
     data.Contents.forEach(item => {
@@ -92,60 +92,64 @@ crontab.scheduleJob("30 15 * * *", () => { // Create a job that runs at midnight
 
 /* Refresh the image predictions and then cache the number of cars detected */
 router.get('/refreshpredictions/:id/:url', function (req, res, next) {
-  const { id } = req.params;
-  const url = req.params.url.replace(/\$/g, '/');
+  try {
+    const { id } = req.params;
+    const url = req.params.url.replace(/\$/g, '/');
 
-  image(url, async function (err, image) {
-    const numChannels = 3;
-    const numPixels = image.width * image.height;
-    const values = new Int32Array(numPixels * numChannels);
-    pixels = image.data;
+    image(url, async function (err, image) {
+      const numChannels = 3;
+      const numPixels = image.width * image.height;
+      const values = new Int32Array(numPixels * numChannels);
+      pixels = image.data;
 
-    for (let i = 0; i < numPixels; i++) {
-      for (let channel = 0; channel < numChannels; ++channel) {
-        values[i * numChannels + channel] = pixels[i * 4 + channel];
+      for (let i = 0; i < numPixels; i++) {
+        for (let channel = 0; channel < numChannels; ++channel) {
+          values[i * numChannels + channel] = pixels[i * 4 + channel];
+        }
       }
-    }
-    const outShape = [image.height, image.width, numChannels];
-    const input = tf.tensor3d(values, outShape, 'int32');
+      const outShape = [image.height, image.width, numChannels];
+      const input = tf.tensor3d(values, outShape, 'int32');
 
-    (await model).detect(input)
-      .then(predictions => {
-        // Only filer out "car" predictions
-        let predictionBoxes = [];
-        predictions.forEach(prediction => {
-          if (prediction.class === "car") {
-            predictionBoxes.push(prediction.bbox);
-          }
-        });
+      (await model).detect(input)
+        .then(predictions => {
+          // Only filer out "car" predictions
+          let predictionBoxes = [];
+          predictions.forEach(prediction => {
+            if (prediction.class === "car") {
+              predictionBoxes.push(prediction.bbox);
+            }
+          });
 
-        cache.set(`${id}:PredictionBoxes`, JSON.stringify(predictionBoxes), 'EX', 180);
+          cache.set(`${id}:PredictionBoxes`, JSON.stringify(predictionBoxes), 'EX', 180);
 
-        // Update the current count by adding what was currently detected on top of the current total
-        cache.get(`${id}:CurrentCount`, function (err, response) {
-          let value;
-          if (response) {
-            value = parseInt(response) + predictionBoxes.length;
-          } else {
-            value = predictionBoxes.length;
-          }
-          cache.set(`${id}:CurrentCount`, value.toString(), 'EX', 180);
-        });
+          // Update the current count by adding what was currently detected on top of the current total
+          cache.get(`${id}:CurrentCount`, function (err, response) {
+            let value;
+            if (response) {
+              value = parseInt(response) + predictionBoxes.length;
+            } else {
+              value = predictionBoxes.length;
+            }
+            cache.set(`${id}:CurrentCount`, value.toString(), 'EX', 180);
+          });
 
-        cache.get(`${id}:DayCount`, function (err, response) {
-          let value;
-          if (response) {
-            value = parseInt(response) + predictionBoxes.length;
-          } else {
-            value = predictionBoxes.length;
-          }
-          cache.set(`${id}:DayCount`, value.toString(), 'EX', 2 * 60 * 60, function (e, r) {
-            res.status(200).send();
+          cache.get(`${id}:DayCount`, function (err, response) {
+            let value;
+            if (response) {
+              value = parseInt(response) + predictionBoxes.length;
+            } else {
+              value = predictionBoxes.length;
+            }
+            cache.set(`${id}:DayCount`, value.toString(), 'EX', 2 * 60 * 60, function (e, r) {
+              res.status(200).send();
+            });
           });
         });
-      })
-      .catch(error => console.log(error));
-  });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send();
+  }
 });
 
 /* Get the current count from cache and the count array from S3, append the current one, update the S3 object with the new value, and delete the item from cache */
